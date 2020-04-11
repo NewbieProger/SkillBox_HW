7f7a51fc4c80ff5fa03b3cc380c7ca5fd0e2c614
mikhail.bolgov <mushah9l@gmail.com>
2020-04-12 02:38:33 +0700

    Cycle For

---
+++ WhileCycle/src/Tickets.java
@@ -0,0 +1,15 @@
+public class Tickets {
+    public static void main(String[] args) {
+
+
+        for (int i = 200000; i <= 210000; i++) {
+            System.out.println(i);
+            if (i == 210000) {
+                for (i = 220000; i <= 235000; i++) {
+                    System.out.println(i);
+                }
+
+            }
+        }
+    }
+}
