961d85be0d4c34877c826ffa92280f445b6dc0b4
mikhail.bolgov <mushah9l@gmail.com>
2020-04-12 02:51:16 +0700

    Cycle While

---
+++ WhileCycle/src/Tickets_While.java
@@ -0,0 +1,18 @@
+public class Tickets_While {
+    public static void main(String[] args) {
+
+        int i = 200000;
+        while (i <= 210000) {
+            System.out.println(i);
+            i++;
+            if (i == 210001) {
+                i = 220000;
+                while (i <= 235000) {
+                    System.out.println(i);
+                    i++;
+                }
+
+            }
+        }
+    }
+}
