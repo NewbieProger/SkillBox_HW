1e9b57130df9ad8e85add01bb31582032d9c0281
mikhail.bolgov <mushah9l@gmail.com>
2020-04-12 22:56:02 +0700

    Комментарии и разбор кода

--- WhileCycle/src/Tickets.java
+++ WhileCycle/src/Tickets.java
@@ -2,14 +2,12 @@ public class Tickets {
     public static void main(String[] args) {
 
 
-        for (int i = 200000; i <= 210000; i++) {
+        for (int i = 200000; i <= 235000; i++) {
             System.out.println(i);
             if (i == 210000) {
-                for (i = 220000; i <= 235000; i++) {
-                    System.out.println(i);
-                }
-
+                i = 219999;
             }
+
         }
     }
 }
