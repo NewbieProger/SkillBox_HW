a6387d99904c65b56f5e08562580d5ce0112e7c9
mikhail.bolgov <mushah9l@gmail.com>
2020-05-01 19:47:43 +0700

    Задание 4.1 Реализация метода sumDigits

--- 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
+++ 04_NumbersStringsAndDates/IntegerExperiments/src/Main.java
@@ -1,15 +1,34 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         Container container = new Container();
         container.count += 7843;
 
+
+        //Нужно создать объект, если метод не статичный
+//        Main main = new Main();
+//        System.out.println(main.sumDigits(321));
+
+        System.out.println(sumDigits(container.count)); //Если бы я сделал метод статичным
     }
 
-    public Integer sumDigits(Integer number)
-    {
-        //@TODO: write code here
-        return 0;
+    public static Integer sumDigits(Integer number) { //1. Сделал статичным, потому что идея так сказала. Видимо, чтобы я смог его вызвать без создания объекта?
+        String str = number.toString(); //Тут создаем объект "Строка", обращаемся к передаваемому number (а он, в свою очередь, является объектом, т.к. является Integer'om, и превращаем в строку
+        int sum = 0; //Просто переменную сделали, чтобы было , что заполнить и к чему обратиться
+        for (int x = 0; x < str.length(); x++) { //Инициализация переменной x, чтобы было от чего считать и она же является индексом строки; Индекс меньше длины строки; Увеличиваем индекс строки
+            int a = Integer.parseInt(String.valueOf(str.charAt(x)));
+//            int a = Integer.parseInt(Integer.toString(str.charAt(x)));
+            /*(изнутри во вне расписываю):
+            * у строки ищем символ по индексу
+            * Этот символ становится типом char, и сначала я написал просто "int a = Integer.parseInt(str.charAt(x));", а ИДЕЯ сказал обернуть в valueOf, потому что parseInt принимает string, а не char
+            * Хотел обернуть в toString(), но у нас статик метод (потом сделал объект Main'a , но у меня начала херота передаваться, а не те цифры, что там есть
+            * Потом, через операнд "+=" складываем сумму цифр
+            * */
+
+            sum += a;
+
+        }
+
+        return sum; //Возвращаем сумму
     }
+
 }
