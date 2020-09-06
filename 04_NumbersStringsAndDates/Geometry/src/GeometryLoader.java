3f91b61d318d0b84dbbdf3ef560c0ea47d19f93f
mikhail.bolgov <mushah9l@gmail.com>
2020-09-07 03:54:18 +0700

    Задание 4.3. Рассчёт всяких формул — подсчёта площади круга, площади треугольника и объёма сферы; — проверка возможности построения треугольника по трём длинам сторон.

---
+++ 04_NumbersStringsAndDates/Geometry/src/GeometryLoader.java
@@ -0,0 +1,67 @@
+public class GeometryLoader {
+
+    public double radius;
+    public double triangleBasis;
+    public double triangleHeight;
+    public double volumeRadius;
+    public double triangleBasisFirst;
+    public double triangleBasisSecond;
+    public double triangleBasisThird;
+
+
+    public double getTriangleBasisFirst() {
+        return triangleBasisFirst;
+    }
+
+    public void setTriangleBasisFirst(double triangleBasisFirst) {
+        this.triangleBasisFirst = triangleBasisFirst;
+    }
+
+    public double getTriangleBasisSecond() {
+        return triangleBasisSecond;
+    }
+
+    public void setTriangleBasisSecond(double triangleBasisSecond) {
+        this.triangleBasisSecond = triangleBasisSecond;
+    }
+
+    public double getTriangleBasisThird() {
+        return triangleBasisThird;
+    }
+
+    public void setTriangleBasisThird(double triangleBasisThird) {
+        this.triangleBasisThird = triangleBasisThird;
+    }
+
+    public double getVolumeRadius() {
+        return volumeRadius;
+    }
+
+    public void setVolumeRadius(double volumeRadius) {
+        this.volumeRadius = volumeRadius;
+    }
+
+    public double getTriangleBasis() {
+        return triangleBasis;
+    }
+
+    public void setTriangleBasis(Double triangleBasis) {
+        this.triangleBasis = triangleBasis;
+    }
+
+    public double getTriangleHeight() {
+        return triangleHeight;
+    }
+
+    public void setTriangleHeight(Double triangleHeight) {
+        this.triangleHeight = triangleHeight;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+}
