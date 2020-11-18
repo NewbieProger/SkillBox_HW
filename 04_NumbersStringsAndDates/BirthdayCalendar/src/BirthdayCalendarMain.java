43cfa199b6b6ac4d8f7a02597afceb33d5eb324a
mikhail.bolgov <mushah9l@gmail.com>
2020-11-19 01:25:42 +0700

    Выполнены задания: 4.5, 4.5*, 4.6

--- 04_NumbersStringsAndDates/BirthdayCalendar/src/BirthdayCalendarMain.java
+++ 04_NumbersStringsAndDates/BirthdayCalendar/src/BirthdayCalendarMain.java
@@ -1,5 +1,8 @@
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.Calendar;
 import java.util.Locale;
 
@@ -14,25 +17,51 @@ public class BirthdayCalendarMain {
 
     }
 
-    public static void collectBirthdays(int year, int month, int day) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.DAY_OF_MONTH, day);
-        calendar.set(Calendar.MONTH, month - 1);
-        int currentYear = calendar.get(Calendar.YEAR);
+//    public static void collectBirthdays(int year, int month, int day) {
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.set(Calendar.DAY_OF_MONTH, day);
+//        calendar.set(Calendar.MONTH, month - 1);
+//        int currentYear = calendar.get(Calendar.YEAR);
+//
+//        int age = 0;
+//
+//        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+//
+//        for (int i = year; i <= currentYear; i++) {
+//            calendar.set(Calendar.YEAR, i);
+//            System.out.println(new StringBuilder().append(age)
+//                    .append(" - ")
+//                    .append(dateFormat.format(calendar.getTime()))
+//                    .append(" - ")
+//                    .append(calendar.getDisplayName(calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US)));
+//
+//            age++;
+//        }
+//    }
 
-        int age = 0;
+        public static void collectBirthdays(int year, int month, int day) {
 
-        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+            int currentYear = LocalDate.now().getYear();
 
-        for (int i = year; i <= currentYear; i++) {
-            calendar.set(Calendar.YEAR, i);
-            System.out.println(new StringBuilder().append(age)
-                    .append(" - ")
-                    .append(dateFormat.format(calendar.getTime()))
-                    .append(" - ")
-                    .append(calendar.getDisplayName(calendar.DAY_OF_WEEK, Calendar.SHORT, Locale.US)));
+            int age = 0;
 
-            age++;
-        }
+            DateTimeFormatter formattedDate = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+
+            for (int i = year; i <= currentYear; i++) {
+
+                LocalDate birthdayEachYear = LocalDate.of(i, month, day);
+                System.out.println();
+
+                System.out.println(new StringBuilder()
+                        .append("Возраст: ")
+                        .append(age)
+                        .append(" - ")
+                        .append(formattedDate.format(birthdayEachYear))
+                        .append(" - ")
+                        .append(birthdayEachYear.getDayOfWeek().toString().substring(0, 3)));
+
+                age++;
+            }
+
     }
 }
