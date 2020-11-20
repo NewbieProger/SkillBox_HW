a3f7c12dc90dcae6eccbf7d6e3e6b8c5f2ea47cc
mikhail.bolgov <mushah9l@gmail.com>
2020-11-21 03:03:03 +0700

    Выполнены задания: 4.5, 4.5*, 4.6

--- 04_NumbersStringsAndDates/FullNameFormatterRegExp/src/TestNameFormatter.java
+++ 04_NumbersStringsAndDates/FullNameFormatterRegExp/src/TestNameFormatter.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TestNameFormatter {
     public static void main(String[] args) {
@@ -9,11 +11,26 @@ public class TestNameFormatter {
                 break;
             }
 
-            if (input.matches("^[А-Яа-я-]+\\s[А-Яа-я-]+\\s[А-Яа-я]+$")) {
-                String[] splitText = input.split(" ");
-                System.out.println("Фамилия: " + splitText[0].trim() + "\r\n" +
-                        "Имя: " + splitText[1].trim() + "\r\n" +
-                        "Отчество: " + splitText[2].trim());
+            String regex = "([А-Яа-я-]+) ([А-Яа-я-]+) ([А-Яа-я-]+)";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(input);
+
+            /*Исходная мною реализация, по ТЗ*/
+//            if (matcher.matches()) {
+//                String[] splitText = input.split(" ");
+//                System.out.println("Фамилия: " + splitText[0].trim() + "\r\n" +
+//                        "Имя: " + splitText[1].trim() + "\r\n" +
+//                        "Отчество: " + splitText[2].trim());
+//
+//            } else System.out.println("Введенная строка не является ФИО");
+
+            if (matcher.matches()) {
+                System.out.println(new StringBuilder("Фамилия: ")
+                        .append(matcher.group(1) + "\n")
+                        .append("Имя: ")
+                        .append(matcher.group(2) + "\n")
+                        .append("Отчество: ")
+                        .append(matcher.group(3)));
 
             } else System.out.println("Введенная строка не является ФИО");
         }
